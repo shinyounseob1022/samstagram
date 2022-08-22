8701e7070d0c4735e25e3056fd35fa881d6c251a
minyoungki <aksrkaksen12@gmail.com>
2022-08-23 00:01:16 +0900

    Like 기능 수정

--- src/main/java/com/sparta/samstagram/service/LikeService.java
+++ src/main/java/com/sparta/samstagram/service/LikeService.java
@@ -34,7 +34,7 @@ public class LikeService {
     public ResponseDto<?> likePost(Long postId, HttpServletRequest request) {
         Member member = validateMember(request);
         if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
+            return ResponseDto.fail("INVALID_TOKEN", "Authorization is invalid");
         }
 
         Post post = postService.isPresentPost(postId);
@@ -73,7 +73,7 @@ public class LikeService {
     public ResponseDto<?> likeComment(Long postId, Long commentId, HttpServletRequest request) {
         Member member = validateMember(request);
         if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
+            return ResponseDto.fail("INVALID_TOKEN", "Authorization is invalid");
         }
 
         Post post = postService.isPresentPost(postId);
